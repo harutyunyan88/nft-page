e64e6ef29c5b305219c263d8c593da1ebeb8a47b
harutyunyan88 <arsen.harutyunyan088@gmail.com>
2023-01-12 16:21:50 +0400

    change router settings

--- src/App.js
+++ src/App.js
@@ -1,17 +1,16 @@
 import "./App.css";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { BrowserRouter, HashRouter, Route, Routes } from "react-router-dom";
 import Home from "./pages/Home/Home";
 import StudyInfo from "./pages/StudyInfo/StudyInfo";
 
 function App() {
   return (
-    <div>test</div>
-    // <BrowserRouter>
-    //   <Routes>
-    //     <Route path="/" element={<Home />} />
-    //     <Route path="/study-information" element={<StudyInfo />} />
-    //   </Routes>
-    // </BrowserRouter>
+    <HashRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/study-information" element={<StudyInfo />} />
+      </Routes>
+    </HashRouter>
   );
 }
 
